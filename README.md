d28bcffee91462e40d8a37e4c88bb658b186af0e
yeabLimtless <tesfayeyeabsra446@gmail.com>
2023-03-04 21:49:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<a href="https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl"
+  >news-homepage-main</a>
+
+<h1>Desktop-design</h1>
+<img src="./design/desktop-design.jpg" />
+<h1>Mobile-Design</h1>
+<img src="./design/mobile-design.jpg" alt="">
+<h1>Mobile-Menu</h1>
+<img src="./design/mobile-menu.jpg" alt="">
---
+++ app.js
@@ -0,0 +1,27 @@
+const links = document.querySelector('.links')
+const menuOpen = document.querySelector('.menu-open')
+const menuClose = document.querySelector('.menu-close')
+const backDrop = document.querySelector(".backdrop");
+
+menuOpen.addEventListener('click', () =>{
+    links.classList.add('active')
+    menuClose.classList.add("active");
+    menuOpen.classList.add("active");
+    backDrop.classList.add("active");
+    
+})
+
+menuClose.addEventListener('click', () =>{
+    links.classList.remove('active')
+    menuClose.classList.remove("active");
+    menuOpen.classList.remove("active");
+    backDrop.classList.remove("active");
+    
+})
+
+backDrop.addEventListener("click", () => {
+  links.classList.remove("active");
+  menuClose.classList.remove("active");
+  menuOpen.classList.remove("active");
+  backDrop.classList.remove("active");
+});
